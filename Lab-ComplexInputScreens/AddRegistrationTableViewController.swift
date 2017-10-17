280d6c5659514ff61f802372f23fadc41d415269
Unknown <kosyak_@mail.ru>
2017-10-17 17:07:54 +0300

    Added segue

--- Lab-ComplexInputScreens/AddRegistrationTableViewController.swift
+++ Lab-ComplexInputScreens/AddRegistrationTableViewController.swift
@@ -246,14 +246,16 @@ class AddRegistrationTableViewController: UITableViewController, SelectRoomTypeT
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "SelectRoomType" {
+            let destinationViewController = segue.destination as? SelectRoomTypeTableViewController
+            destinationViewController?.delegate = self
+            destinationViewController?.roomType = roomType
+        }
     }
-    */
+    
 
 }
